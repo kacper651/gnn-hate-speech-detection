ae30503fd5753f8a2fd499979dbcb7b23113d6b6
kacper651 <kacper.wroblewski651@gmail.com>
2025-06-06 13:43:08 +0200

    add outputs

--- src/GNNs/GCNClassifier.py
+++ src/GNNs/GCNClassifier.py
@@ -1,4 +1,4 @@
-from torch.nn import Linear, ReLU, Module
+from torch.nn import Linear, ReLU, Module, Dropout
 from torch_geometric.nn import GCNConv, global_mean_pool
 
 class GCNClassifier(Module):
@@ -8,10 +8,12 @@ class GCNClassifier(Module):
         self.conv2 = GCNConv(hidden_channels, hidden_channels)
         self.relu = ReLU()
         self.lin = Linear(hidden_channels, num_classes)
+        self.dropout = Dropout(p=0.5)
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         x = self.relu(self.conv1(x, edge_index))
         x = self.relu(self.conv2(x, edge_index))
-        x = global_mean_pool(x, batch)  # Graph-level embedding
+        x = global_mean_pool(x, batch)
+        x = self.dropout(x)
         return self.lin(x)
--- src/embeddings/sbert.py
+++ src/embeddings/sbert.py
@@ -1,13 +1,11 @@
 from torch import Tensor
-from torch.nn import Module
 from sentence_transformers import SentenceTransformer
 
-class SBERTEmbedding(Module):
+class SBERTEmbedding():
     def __init__(self, model_name='all-MiniLM-L6-v2', device='cpu'):
-        super().__init__()
-        self.device = device
-        self.model = SentenceTransformer(model_name).to(device)
+        self.model = SentenceTransformer(model_name, device=device)
 
-    def forward(self, text: str) -> Tensor:
-        embedding = self.model.encode(text, convert_to_tensor=True)
-        return embedding.to(self.device)
+    def embed_words(self, words: str) -> Tensor:
+        words = words.split()
+        embeddings = self.model.encode(words, convert_to_tensor=True)
+        return embeddings
